ca7653fd30e1fb48a983beaa9b0f8cc54f379f67
Michał Witczak <michalwitczak19@gmail.com>
2021-06-03 22:55:26 +0200

    Add project files.

---
+++ Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AplOkien
+{
+    public class Student
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+        public int NrIndeksu { get; set; }
+        public string Wydzial { get; set; }
+
+        public Student(string imie, string nazwisko, int nrIndeksu, string wydzial)
+        {
+            Imie = imie;
+            Nazwisko = nazwisko;
+            NrIndeksu = nrIndeksu;
+            Wydzial = wydzial;
+
+        }
+
+        public Student()
+            :this("","",0,"")
+        {
+            
+        }
+
+    }
+}
